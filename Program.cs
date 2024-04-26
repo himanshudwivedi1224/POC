72cfcd70ccd57a6ac56f4f5daaf1db400e6c7c84
Himanshu Dwivedi <54054434+himanshudwivedi1224@users.noreply.github.com>
2024-04-26 21:29:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Tesseract;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Replace "path/to/your/image.png" with the path to your image file
+        string imagePath = "C:\\Users\\ZK618AP\\Downloads\\unnamed.png";
+
+        try
+        {
+
+
+            using (var engine = new TesseractEngine("C:\\Users\\ZK618AP\\AppData\\Local\\Programs\\Tesseract-OCR\\tessdata", "eng", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(imagePath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        var text = page.GetText();
+                        Console.WriteLine("OCR Result:");
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        Console.ReadLine();
+    }
+}
